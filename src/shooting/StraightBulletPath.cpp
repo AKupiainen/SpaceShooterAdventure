214aaee1ac09f8084fe830bcb16ff9b4b70a78ce
Aki Kupiainen <kupiainen.aki@gmail.com>
2025-03-16 14:31:22 +0200

    Remove comments

--- src/shooting/StraightBulletPath.cpp
+++ src/shooting/StraightBulletPath.cpp
@@ -10,14 +10,11 @@ StraightBulletPath::StraightBulletPath(double speedX, double speedY)
 void StraightBulletPath::Update(double& posX, double& posY, double rotationAngle) {
     double deltaTime = Time::GetDeltaTime();
 
-    // Convert angle from degrees to radians
     double radAngle = rotationAngle * DEG_TO_RAD;
 
-    // If rotationAngle is non-zero, adjust the velocity direction accordingly
     double velocityX = speedX * cos(radAngle) - speedY * sin(radAngle);
     double velocityY = speedX * sin(radAngle) + speedY * cos(radAngle);
 
-    // Apply movement update
     posX += velocityX * deltaTime;
     posY += velocityY * deltaTime;
 }
